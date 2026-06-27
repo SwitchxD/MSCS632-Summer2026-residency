b654854cfc5b0e6e22e6bf5300a122cb2ed56db8
Lokesh <lokeshmishra2@gmail.com>
2026-06-27 08:38:19 -0700

    Java springboot initialize

---
+++ Java/team5Residency/src/test/java/com/team5/team5residency/Team5ResidencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.team5residency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5ResidencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
